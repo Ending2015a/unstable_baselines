9d00b3c4f506b3c5117aacccc2d79930d899d8aa
Joe Hsiao <joehsiao@gapp.nthu.edu.tw>
2021-03-19 21:05:38 +0800

    add print_scores.py

--- print_scores.py
+++ print_scores.py
@@ -8,21 +8,22 @@ from pathlib import Path
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='Print Score')
   parser.add_argument('--path', type=str, default='log/', help='Root dir')
-  parser.add_argument('--glob', type=str, default='*.json', help='file to glob')
+  parser.add_argument('--file', type=str, default='*.json', help='filename')
+  parser.add_argument('--reverse', action='store_true', help='Reverse sorting')
 
   a = parser.parse_args()
 
-  json_files = sorted([x for x in Path(a.path).rglob(a.glob)])
+  json_files = sorted([x for x in Path(a.path).rglob(a.file)])
   j_pairs = []
 
   for json_file in json_files:
-    with open(json_file, 'r') as f:
+    with open(str(json_file), 'r') as f:
       j = json.load(f)
       if 'monitor' in j and 'episode_rewards' in j['monitor']:
         j_pairs.append( (json_file, j) )
       
 
-  j_pairs = sorted(j_pairs, key=lambda x:x[1]['monitor']['episode_rewards'], reverse=True)
+  j_pairs = sorted(j_pairs, key=lambda x:x[1]['monitor']['episode_rewards'], reverse=a.reverse)
 
   for j_pair in j_pairs:
     json_file = j_pair[0]
