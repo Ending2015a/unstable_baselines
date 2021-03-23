3d243b84540f4ac085c05e59e166ecc190fe2bf8
Joe Hsiao <joehsiao@gapp.nthu.edu.tw>
2021-03-23 11:03:07 +0800

    td complete

--- unstable_baselines/td3/train.sh
+++ unstable_baselines/td3/train.sh
@@ -5,7 +5,7 @@ seed=$2
 
 
 python -m unstable_baselines.td3.run \
-            --rank $rank --seed $seed --logdir='./log/{env_id}/td3_1m/{rank}' --logging='training.log' \
+            --rank $rank --seed $seed --logdir='./log/{env_id}/td3/{rank}' --logging='training.log' \
             --monitor_dir='monitor' --tb_logdir='' --model_dir='model' \
             --env_id="HalfCheetahBulletEnv-v0" --num_envs=1 --num_episodes=1000 --min_buffer=10000 \
             --num_steps=1000 --gradient_steps=1000 --batch_size=100 --verbose=2
