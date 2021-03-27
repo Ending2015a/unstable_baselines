6951ad33814e500b58c493f93177b2bdf8f9e47b
Joe Hsiao <joehsiao@gapp.nthu.edu.tw>
2021-03-27 16:18:56 +0800

    add some log

--- unstable_baselines/ppo/run.py
+++ unstable_baselines/ppo/run.py
@@ -253,6 +253,7 @@ if __name__ == '__main__':
         LOG.info('DONE')
 
         # Save complete model (continue training)
+        LOG.info('Saving model to: {}'.format(a.model_dir))
         model.save(a.model_dir)
         loaded_model = PPO.load(a.model_dir)
 
@@ -267,10 +268,12 @@ if __name__ == '__main__':
         #                     eval_max_steps = a.eval_max_steps)
 
         # Save agent only
+        # LOG.info('Saving model to: {}'.format(a.model_dir))
         # model.agent.save(a.model_dir)
         # loaded_model = PPOAgent.load(a.model_dir)
 
         # Evaluation
+        LOG.info('Evaluating model')
         eps_rews  = []
         eps_steps = []
         for episode in range(a.eval_episodes):
