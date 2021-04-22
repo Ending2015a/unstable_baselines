187b795e324a30a030a2e4e58d64e350414e94ac
Ending2015a <joehsiao@gapp.nthu.edu.tw>
2021-04-22 23:22:14 +0800

    add algo qrdqn, not tested yet

---
+++ unstable_baselines/d/qrdqn/README.md
@@ -0,0 +1,91 @@
+# Quantile Regression Deep Q-learning (QRDQN)
+
+> [Dabney, W., Rowland, M., Bellemare, M., & Munos, R. (2018). Distributional Reinforcement Learning With Quantile Regression. *In AAAI Conference on Artificial Intelligence.*](https://arxiv.org/abs/1710.10044)
+
+
+## How to use
+
+### Run with default arguments
+```python
+./unstable_baselines/d/qrdqn/train.sh --rank 0 --seed 1 "BreakoutNoFrameskip-v4"
+```
+
+### Run multiple environments with default arguments
+```python
+./unstable_baselines/d/qrdqn/train.sh --rank 0 --seed 1 "BreakoutNoFrameskip-v4" "SeaquestNoFrameskip-v4"
+```
+
+### Atari-like environment (Image observation + discrete action)
+```python
+python -m unstable_baselines.d.qrdqn.run --rank 0 --seed 1 --logdir='./log/{env_id}/dqn/{rank}' \
+               --logging='training.log' --monitor_dir='monitor' --tb_logdir='' --model_path='model/weights' \
+               --env_id="BreakoutNoFrameskip-v4" --num_envs=8 --num_epochs=312500 \
+               --num_steps=4 --num_gradsteps=1 --batch_size=256 --target_update=625 \
+               --explore_rate=1.0 --explore_final=0.05 --explore_progress=0.1 \
+               --num_quantiles=200 --record_video
+```
+
+<sup>Total timesteps (Samples) ≈ num_envs * num_steps * num_epochs (~10M in this case)</sup><br>
+<sup>Number of times each sample reused ≈ batch_size/num_steps * num_gradsteps/num_envs (~8 in this case)</sup><br>
+
+## Atari 2600
+
+### Video
+
+| `BeamRiderNoFrameskip-v4` | `BreakoutNoFrameskip-v4` | `PongNoFrameskip-v4` | `SeaquestNoFrameskip-v4` |
+|---------------------------|--------------------------|----------------------|--------------------------|
+|<img src="https://github.com/Ending2015a/unstable_baselines_assets/blob/master/images/qrdqn.BeamRiderNoFrameskip-v4.eval.gif" height=300px>|<img src="https://github.com/Ending2015a/unstable_baselines_assets/blob/master/images/qrdqn.BreakoutNoFrameskip-v4.eval.gif" height=300px>|<img src="https://github.com/Ending2015a/unstable_baselines_assets/blob/master/images/qrdqn.PongNoFrameskip-v4.eval.gif" height=300px>|<img src="https://github.com/Ending2015a/unstable_baselines_assets/blob/master/images/qrdqn.SeaquestNoFrameskip-v4.eval.gif" height=300px>|
+| `AsteroidsNoFrameskip-v4` | `EnduroNoFrameskip-v4`   | `QbertNoFrameskip-v4` | `MsPacmanNoFrameskip-v4` |
+|<img src="https://github.com/Ending2015a/unstable_baselines_assets/blob/master/images/qrdqn.AsteroidsNoFrameskip-v4.eval.gif" height=300px>|<img src="https://github.com/Ending2015a/unstable_baselines_assets/blob/master/images/qrdqn.EnduroNoFrameskip-v4.eval.gif" height=300px>|<img src="https://github.com/Ending2015a/unstable_baselines_assets/blob/master/images/qrdqn.QbertNoFrameskip-v4.eval.gif" height=300px>|<img src="https://github.com/Ending2015a/unstable_baselines_assets/blob/master/images/qrdqn.MsPacmanNoFrameskip-v4.eval.gif" height=300px>|
+
+### Results
+
+> Learning curve
+
+| `env_id`                  | Max rewards | Mean rewards | Std rewards | Total frames | Eval episodes |
+|---------------------------|------------:|-------------:|------------:|-------------:|--------------:|
+| `AsteroidsNoFrameskip-v4` |             |              |             |          10M |            20 |
+| `BeamRiderNoFrameskip-v4` |       11500 |       7791.1 |     2570.17 |          10M |            20 |
+| `BreakoutNoFrameskip-v4`  |         424 |       393.05 |          25 |          10M |            20 |
+| `EnduroNoFrameskip-v4`    |        2229 |       1726.1 |      310.23 |          10M |            20 |
+| `MsPacmanNoFrameskip-v4`  |             |              |             |          10M |            20 |
+| `PongNoFrameskip-v4`      |          21 |         20.8 |         0.6 |          10M |            20 |
+| `QbertNoFrameskip-v4`     |             |              |             |          10M |            20 |
+| `SeaquestNoFrameskip-v4`  |        9660 |         8307 |      665.29 |          10M |            20 |
+
+<sup>M = million (1e6)</sup><br>
+
+### Hyperparameters
+
+
+| `env_id`           | `AsteroidsNoFrameskip-v4` | `BeamRiderNoFrameskip-v4` | `BreakoutNoFrameskip-v4` | `EnduroNoFrameskip-v4` | `MsPacmanNoFrameskip-v4` | `PongNoFrameskip-v4` | `QbertNoFrameskip-v4` | `SeaquestNoFrameskip-v4` |
+|--------------------|:-------------------------:|:-------------------------:|:------------------------:|:----------------------:|:------------------------:|:--------------------:|:---------------------:|:------------------------:|
+| `num_envs`         |             8             |             8             |             8            |            8           |             8            |           8          |           8           |             8            |
+| `num_epochs`       |           312500          |           312500          |          312500          |         312500         |          312500          |        312500        |         312500        |          312500          |
+| `num_steps`        |             4             |             4             |             4            |            4           |             4            |           4          |           4           |             4            |
+| `num_gradsteps`    |             1             |             1             |             1            |            1           |             1            |           1          |           1           |             1            |
+| `batch_size`       |            256            |            256            |            256           |           256          |            256           |          256         |          256          |            256           |
+| `target_update`    |            625            |            625            |            625           |           625          |            625           |          625         |          625          |            625           |
+| `exploration`      |     Linear(1.0, 0.01)     |     Linear(1.0, 0.01)     |     Linear(1.0, 0.01)    |    Linear(1.0, 0.01)   |     Linear(1.0, 0.01)    |   Linear(1.0, 0.01)  |   Linear(1.0, 0.01)   |     Linear(1.0, 0.01)    |
+| `explore_progress` |            0.1            |            0.1            |            0.1           |           0.1          |            0.1           |          0.1         |          0.1          |            0.1           |
+| `num_quantiles`    |            200            |            200            |            200           |           200          |            200           |          200         |          200          |            200           |
+
+## Architecture
+
+|             | `Box`              | `Discrete`         | `MultiDiscrete` | `MultiBinary` |
+|:-----------:|:------------------:|:------------------:|:---------------:|:-------------:|
+| Observation | :heavy_check_mark: | :x:                | :x:             | :x:           |
+| Action      | :x:                | :heavy_check_mark: | :x:             | :x:           |
+
+<br/>
+<br/>
+
+### Atari-like environment
+![](https://g.gravizo.com/source/svg/qrdqn_discrete?https%3A%2F%2Fraw.githubusercontent.com%2FEnding2015a%2Funstable_baselines_assets%2Fmaster%2Fscripts%2Farch%2Fqrdqn.arch.md)
+<br/>
+<sup>force_mlp=`False`</sup><br>
+
+
+![](https://g.gravizo.com/source/svg/qrdqn_continuous?https%3A%2F%2Fraw.githubusercontent.com%2FEnding2015a%2Funstable_baselines_assets%2Fmaster%2Fscripts%2Farch%2Fqrdqn.arch.md)
+<br/>
+<sup>force_mlp=`True`</sup><br>
---
+++ unstable_baselines/d/qrdqn/run.py
@@ -0,0 +1,292 @@
+__copyright__ = '''
+ The MIT License (MIT)
+ Copyright (c) 2021 Joe Hsiao
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+ OR OTHER DEALINGS IN THE SOFTWARE.
+'''
+__license__ = 'MIT'
+
+
+# --- built in ---
+import os
+import sys
+import time
+import logging
+import argparse
+
+# --- 3rd party ---
+import gym
+
+import numpy as np
+
+# --- my module ---
+from unstable_baselines import logger
+
+from unstable_baselines.envs import *
+from unstable_baselines.utils import set_global_seeds
+from unstable_baselines.sche import LinearScheduler
+
+from unstable_baselines.d.qrdqn.model import QRDQN
+from unstable_baselines.d.qrdqn.model import Agent as QRDQNAgent
+
+def parse_args():
+
+    parser = argparse.ArgumentParser(description='Quantile Regression DQN (QRDQN)')
+    parser.add_argument('--logdir',           type=str, default='log/{env_id}/qrdqn/{rank}',help='Root dir             (args: {env_id}, {rank})')
+    parser.add_argument('--logging',          type=str, default='train.log',                help='Log path             (args: {env_id}, {rank})')
+    parser.add_argument('--log_level',        type=str, default='INFO',                     help='Log level')
+    parser.add_argument('--monitor_dir',      type=str, default='monitor',                  help='Monitor dir          (args: {env_id}, {rank})')
+    parser.add_argument('--tb_logdir',        type=str, default='',                         help='Tensorboard log name (args: {env_id}, {rank})')
+    parser.add_argument('--model_path',       type=str, default='model/weights',            help='Model save path      (args: {env_id}, {rank})')
+    parser.add_argument('--env_id',           type=str, default='BeamRiderNoFrameskip-v0',  help='Environment ID')
+    parser.add_argument('--num_envs',         type=int, default=4,      help='Number of environments')
+    parser.add_argument('--num_epochs',       type=int, default=625000, help='Number of training epochs')
+    parser.add_argument('--num_steps',        type=int, default=4,      help='Number of timesteps per epoch (interact with envs)')
+    parser.add_argument('--num_gradsteps',    type=int, default=1,      help='Number of gradient steps per epoch (perform gradient update)')
+    parser.add_argument('--num_quantiles',    type=int, default=200,    help='Number of quantiles of Q value distributions')
+    parser.add_argument('--target_update',    type=int, default=2500,   help='Target network update frequency (gradsteps)')
+    parser.add_argument('--batch_size',       type=int, default=64,     help='Training batch size')
+    parser.add_argument('--buffer_size',      type=int, default=1000000,help='Replay buffer capacity')
+    parser.add_argument('--min_buffer',       type=int, default=50000,  help='Minimum size of replay buffer before start training')
+    parser.add_argument('--verbose',          type=int, default=1,      help='Print more message, 0=less, 1=more train log, 2=more eval log')
+    parser.add_argument('--rank',             type=int, default=0,      help='Optional arguments for parallel training')
+    parser.add_argument('--seed',             type=int, default=0,      help='Random seed')
+    parser.add_argument('--log_interval',     type=int, default=1000,   help='Logging interval (epochs)')
+    parser.add_argument('--eval_interval',    type=int, default=10000,  help='Evaluation interval (epochs)')
+    parser.add_argument('--eval_episodes',    type=int, default=5,      help='Number of episodes each evaluation')
+    parser.add_argument('--eval_max_steps',   type=int, default=3000,   help='Maximum timesteps in each evaluation episode')
+    parser.add_argument('--eval_seed',        type=int, default=0,      help='Environment seed for evaluation')
+    parser.add_argument('--save_interval',    type=int, default=10000,  help='Model checkpoint interval (epochs)')
+    parser.add_argument('--lr',               type=float, default=3e-4, help='Learning rate')
+    parser.add_argument('--gamma',            type=float, default=0.99, help='Gamma decay rate')
+    parser.add_argument('--tau',              type=float, default=1.0,  help='Polyak update rate')
+    parser.add_argument('--kappa',            type=float, default=1.0,  help='Kappa')
+    parser.add_argument('--max_grad_norm',    type=float, default=0.5,  help='Gradient clipping range')
+    parser.add_argument('--explore_rate',     type=float, default=1.0,  help='Initial exploration rate for epsilon greedy')
+    parser.add_argument('--explore_final',    type=float, default=0.05, help='Final exploration rate for epsilon greedy')
+    parser.add_argument('--explore_progress', type=float, default=0.1,  help='Progress to stop decaying exploration rate')
+    parser.add_argument('--force_mlp',        action='store_true',      help='Use MLP network')
+    parser.add_argument('--record_video',     action='store_true',      help='Enable video recording')
+
+    a = parser.parse_args()
+
+    a.logdir      = a.logdir.format(env_id=a.env_id, rank=a.rank)
+    a.logging     = os.path.join(a.logdir, a.logging).format(env_id=a.env_id,     rank=a.rank)
+    a.monitor_dir = os.path.join(a.logdir, a.monitor_dir).format(env_id=a.env_id, rank=a.rank)
+    a.tb_logdir   = os.path.join(a.logdir, a.tb_logdir).format(env_id=a.env_id,   rank=a.rank)
+    a.model_path  = os.path.join(a.logdir, a.model_path).format(env_id=a.env_id,  rank=a.rank)
+
+    return a
+
+def make_atari(a, eval=False):
+    '''
+    Make Atari-like env (Image observation)
+    '''
+    if not eval:
+        def _make_env(rank, a):
+            def _init():
+                logger.Config.use(filename=a.logging, level=a.log_level, 
+                                  colored=True, reset=True)
+                env = gym.make(a.env_id)
+                env.seed(a.seed + rank)
+                env = NoopResetEnv(env, noop_max=30)
+                env = MaxAndSkipEnv(env, skip=4)
+                env = Monitor(env, directory=a.monitor_dir, prefix=str(rank),
+                            enable_video_recording=a.record_video, force=True,
+                            video_kwargs={'prefix':'video/train.{}'.format(rank)})
+                env = EpisodicLifeEnv(env)
+                env = WarpFrame(env)
+                env = ClipRewardEnv(env)
+                return env
+            set_global_seeds(a.seed)
+            return _init
+        env = SubprocVecEnv([_make_env(i, a) for i in range(a.num_envs)])
+        env = VecFrameStack(env, 4)
+    else:
+        env = gym.make(a.env_id)
+        env.seed(a.eval_seed)
+        env = NoopResetEnv(env, noop_max=30)
+        env = MaxAndSkipEnv(env, skip=4)
+        env = Monitor(env, directory=a.monitor_dir, prefix='eval',
+                        enable_video_recording=a.record_video, force=True,
+                        video_kwargs={'prefix':'video/eval',
+                                        'callback': lambda x: True})
+        env = WarpFrame(env)
+        env = FrameStack(env, 4)
+    
+    return env
+
+
+
+if __name__ == '__main__':
+
+    a = parse_args()
+
+    # === Reset logger ===
+    logger.Config.use(filename=a.logging, level=a.log_level, colored=True, reset=True)
+    LOG = logger.getLogger('QRDQN')
+
+    # === Print welcome message ===
+    LOG.add_row('')
+    LOG.add_rows('QRDQN', fmt='{:@f:ANSI_Shadow}', align='center')
+    LOG.add_line()
+    LOG.add_rows('{}'.format(__copyright__))
+    LOG.flush('INFO')
+    time.sleep(1)
+
+    # === Print arguments ===
+    LOG.set_header('Arguments')
+    LOG.add_row('Log dir',               a.logdir)
+    LOG.add_row('Logging path',          a.logging)
+    LOG.add_row('Monitor path',          a.monitor_dir)
+    LOG.add_row('Tensorboard path',      a.tb_logdir)
+    LOG.add_row('Model path',            a.model_path)
+    LOG.add_row('Env ID',                a.env_id)
+    LOG.add_row('Seed',                  a.seed)
+    LOG.add_row('Eval seed',             a.eval_seed)
+    LOG.add_row('Record video',          a.record_video)
+    LOG.add_line()
+    LOG.add_row('Num of envs',           a.num_envs)
+    LOG.add_row('Num of steps/epoch',    a.num_steps)
+    LOG.add_row('Num of epochs',         a.num_epochs)
+    LOG.add_row('Num of gradsteps',      a.num_gradsteps)
+    LOG.add_row('Target update freq',    a.target_update)
+    LOG.add_row('Log interval',          a.log_interval)
+    LOG.add_row('Eval interval',         a.eval_interval)
+    LOG.add_row('Eval episodes',         a.eval_episodes)
+    LOG.add_row('Eval max steps',        a.eval_max_steps)
+    LOG.add_row('Save interval',         a.save_interval)
+    LOG.add_row('Batch size',            a.batch_size)
+    LOG.add_row('Buffer size',           a.buffer_size)
+    LOG.add_row('Min buffer',            a.min_buffer)
+    LOG.add_row('Verbose',               a.verbose)
+    LOG.add_line()
+    LOG.add_row('Force MLP',             a.force_mlp)
+    LOG.add_row('Num of quantiles',      a.num_quantiles)
+    LOG.add_row('Learning rate',         a.lr)
+    LOG.add_row('Gamma',                 a.gamma)
+    LOG.add_row('Tau (Polyak)',          a.tau)
+    LOG.add_row('Kappa',                 a.kappa)
+    LOG.add_row('Max gradient norm',     a.max_grad_norm)
+    LOG.add_row('Explore rate',          a.explore_rate)
+    LOG.add_row('Final explore rate',    a.explore_final)
+    LOG.add_row('Explore progress',      a.explore_progress)
+    LOG.flush('WARNING')
+
+    # === Make envs ===
+
+    # Atari env
+    env      = make_atari(a, eval=False)
+    eval_env = make_atari(a, eval=True)
+    
+    LOG.debug('Action space: {}'.format(env.action_space))
+    LOG.debug('Observation space: {}'.format(env.observation_space))
+
+    # === Create scheduler ===
+    Unit = LinearScheduler.Unit
+    explore_schedule = LinearScheduler(start_value = a.explore_rate,
+                                       stop_value  = a.explore_final,
+                                       decay_steps = a.explore_progress,
+                                       unit        = Unit.progress)
+
+    # === Create model ===
+    try:
+        model = C51(env, learning_rate    = a.lr,
+                         buffer_size      = a.buffer_size,
+                         min_buffer       = a.min_buffer,
+                         n_quantiles      = a.num_quantiles,
+                         n_steps          = a.num_steps,
+                         n_gradsteps      = a.num_gradsteps,
+                         batch_size       = a.batch_size,
+                         gamma            = a.gamma,
+                         tau              = a.tau,
+                         kappa            = a.kappa,
+                         max_grad_norm    = a.max_grad_norm,
+                         force_mlp        = a.force_mlp,
+                         explore_schedule =   explore_schedule,
+                         verbose          = a.verbose)
+        
+        # Total timesteps = num_steps * num_envs * num_epochs (default ~ 10M)
+        model.learn(a.num_steps *    a.num_envs * a.num_epochs, 
+                    tb_logdir      = a.tb_logdir, 
+                    log_interval   = a.log_interval,
+                    eval_env       =   eval_env, 
+                    eval_interval  = a.eval_interval, 
+                    eval_episodes  = a.eval_episodes, 
+                    eval_max_steps = a.eval_max_steps,
+                    save_interval  = a.save_interval,
+                    save_path      = a.model_path,
+                    target_update  = a.target_update)
+        
+        LOG.info('DONE')
+
+        # Save complete model (continue training)
+        saved_path = model.save(a.model_path)
+        LOG.info('Saving model to: {}'.format(saved_path))
+
+        loaded_model = QRDQN.load(saved_path)
+
+        # set env to continue training
+        # loaded_model.set_env(env)
+        # loaded_model.learn(a.num_steps *    a.num_envs * a.num_episodes * 2,
+        #                     tb_logdir      = a.tb_logdir,
+        #                     log_interval   = a.log_interval,
+        #                     eval_env       =  eval_env, 
+        #                     eval_interval  = a.eval_interval, 
+        #                     eval_episodes  = a.eval_episodes, 
+        #                     eval_max_steps = a.eval_max_steps)
+
+        # Save agent only
+        # saved_path = model.agent.save(a.model_path)
+        # LOG.info('Saving model to: {}'.format(saved_path))
+        # loaded_model = QRDQNAgent.load(saved_path)
+
+        # Evaluate model
+        LOG.info('Evaluating model')
+        
+        eps_rews, eps_steps = loaded_model.eval(eval_env, n_episodes=20)
+
+        max_idx    = np.argmax(eps_rews)
+        max_rews   = eps_rews[max_idx]
+        max_steps  = eps_steps[max_idx]
+        mean_rews  = np.mean(eps_rews)
+        std_rews   = np.std(eps_rews)
+        mean_steps = np.mean(eps_steps)
+
+        # === Print evaluation results ===
+        LOG.set_header('Final Evaluation Results')
+        LOG.add_line()
+        LOG.add_row('Max rewards',  max_rews)
+        LOG.add_row('  Length',     max_steps)
+        LOG.add_line()
+        LOG.add_row('Mean rewards', mean_rews)
+        LOG.add_row('Std rewards',  std_rews, fmt='{}: {:.6f}')
+        LOG.add_row('Mean length',  mean_steps)
+        LOG.add_line()
+        LOG.flush('INFO')
+
+    except:
+        LOG.exception('Exception occurred')
+    
+        env.close()
+        eval_env.close()
+        exit(1)
+    
+    env.close()
+    eval_env.close()
