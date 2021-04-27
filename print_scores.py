d306cb21d142c35f69df069f13b8f0dbf6af8665
Ending2015a <joehsiao@gapp.nthu.edu.tw>
2021-04-27 13:16:53 +0800

    update print score script

--- print_scores.py
+++ print_scores.py
@@ -4,6 +4,8 @@ import argparse
 
 from pathlib import Path
 
+import shutil
+
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='Print Score')
@@ -17,18 +19,26 @@ if __name__ == '__main__':
   j_pairs = []
 
   for json_file in json_files:
-    with open(str(json_file), 'r') as f:
-      j = json.load(f)
-      if 'monitor' in j and 'episode_rewards' in j['monitor']:
-        j_pairs.append( (json_file, j) )
-      
+    try:
+      with open(str(json_file), 'r') as f:
+        j = json.load(f)
+        if 'episode_info' in j and 'episode_rewards' in j['episode_info']:
+          j_pairs.append( (json_file, j) )
+    except:
+      print('skip: {}'.format(json_file))
 
-  j_pairs = sorted(j_pairs, key=lambda x:x[1]['monitor']['episode_rewards'], reverse=a.reverse)
+  j_pairs = sorted(j_pairs, key=lambda x:x[1]['episode_info']['episode_rewards'], reverse=a.reverse)
+
+  json_tuples = []
 
   for j_pair in j_pairs:
     json_file = j_pair[0]
     j = j_pair[1]
     
-    rews = j['monitor']['episode_rewards']
-    steps = j['monitor']['episode_length']
+    rews = j['episode_info']['episode_rewards']
+    steps = j['episode_info']['episode_length']
     print('{:8.1f}, {:6d}, {}'.format(rews, steps, json_file))
+
+    json_tuples.append((rews, steps, str(json_file)))
+
+  rews, steps, json_files = zip(*json_tuples)
