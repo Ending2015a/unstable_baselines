13a8c328bd3102aff05833b7382d1e95babdeb39
Ending2015a <joehsiao@gapp.nthu.edu.tw>
2021-04-27 16:42:17 +0800

    update sd3 train script

--- unstable_baselines/sd3/train.sh
+++ unstable_baselines/sd3/train.sh
@@ -24,8 +24,8 @@ function train() {
                     --model_dir='model' \
                     --env_id=$3 \
                     --num_envs=1 \
-                    --num_epochs=10000 \
-                    --num_steps=100 \
+                    --num_epochs=2000 \
+                    --num_steps=1000 \
                     --num_gradsteps=100 \
                     --batch_size=100 \
                     --buffer_size=1000000 \
@@ -33,11 +33,11 @@ function train() {
                     --policy_update=2 \
                     --target_update=2 \
                     --lr=1e-3 \
-                    --log_interval=100 \
-                    --eval_interval=500 \
+                    --log_interval=10 \
+                    --eval_interval=50 \
                     --eval_episodes=5 \
                     --eval_max_steps=1000 \
-                    --save_interval=500 \
+                    --save_interval=50 \
                     --verbose=1 \
                     --action_samples=50 \
                     --action_noise=0.2 \
