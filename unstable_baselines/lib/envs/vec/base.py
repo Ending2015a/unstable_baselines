f61ffdf5aad1abe77f5c7a03bdac6337aa321cc7
Ending2015a <joehsiao@gapp.nthu.edu.tw>
2021-08-11 23:16:54 +0800

    add vecenv, base class, dummy and subproc

--- unstable_baselines/lib/envs/vec/base.py
+++ unstable_baselines/lib/envs/vec/base.py
@@ -1,38 +1,222 @@
 # --- built in ---
 import os
+import abc
 import sys
+
+from typing import Union
 
 # --- 3rd party ---
 import gym
 import numpy as np
 
 # --- my module ---
+from unstable_baselines.lib import utils as ub_utils
+
+[lambda i=j:j for j in range(10)]
+
+# The implementation mainly follows tianshou/venvs.py
+# habitat-lab/vector_env.py and stable-baselines3/subproc_vec_env.py
+class BaseEnvWorker(metaclass=abc.ABCMeta):
+    def __init__(self, env_fn):
+        self._env_fn = env_fn
+        self.closed = False
+        self.observation_space = self.getattr('observation_space')
+        self.action_space      = self.getattr('action_space')
+        self.metadata          = self.getattr('metadata')
+        self.reward_range      = self.getattr('reward_range')
+        self.spec              = self.getattr('spec')
+
+    @abc.abstractmethod
+    def getattr(self, attrname: str):
+        pass
+
+    @abc.abstractmethod
+    def setattr(self, attrname: str, value):
+        pass
+
+    @abc.abstractmethod
+    def reset(self, **kwargs):
+        pass
+
+    @abc.abstractmethod
+    def step_async(self, act):
+        pass
+
+    @abc.abstractmethod
+    def step_wait(self):
+        pass
+
+    @abc.abstractmethod
+    def seed(self, seed):
+        self.action_space.seed(seed)
+
+    @abc.abstractmethod
+    def render(self):
+        pass
+
+    @abc.abstractmethod
+    def close_async(self):
+        pass
+    
+    @abc.abstractmethod
+    def close_wait(self):
+        pass
 
 
-class BaseVecEnvWorker():
-
-
+# The implementation mainly follows tianshou/venvs.py
+# habitat-lab/vector_env.py and stable-baselines3/subproc_vec_env.py
 class BaseVecEnv(gym.Env):
-    def __init__(self, 
-        env_fns: list, 
-        worker_class: BaseVecEnvWorker,
-        vec_norm: bool = False,
+    def __init__(self,
+        env_fns: list,
+        worke_class: BaseEnvWorker,
+        rms_norm: Union[str, bool, ub_utils.RMSNormalizer] = None
     ):
-        metadata
-        reward_range
-        spec
-        action_space
-        observation_space
+        self._worker_class = worker_class
+        self.workers       = [worker_class(fn) for fn in env_fns]
 
+        # init properties
+        worker = self.workers[0]
+        self.observation_space = worker.observation_space
+        self.action_space      = worker.action_space
+        self.metadata          = worker.metadata
+        self.reward_range      = worker.reward_range
+        self.spec              = worker.spec
+        self.n_envs            = len(self.workers)
 
+        self.observation_spaces = [w.observation_space for w in self.workers]
+        self.action_spaces      = [w.action_space for w in self.workers]
+        self.metadatas          = [w.metadata for w in self.workers]
+        self.reward_ranges      = [w.reward_range for w in self.workers]
+        self.specs              = [w.spec for w in self.workers]
 
+        self._rms_norm = self._get_rms_norm_opt(rms_norm)
+        self.closed = False
 
-class VecEnvWorker(BaseVecEnvWorker):
+    @property
+    def rms_norm(self):
+        return self._rms_norm
 
-class VecEnv(BaseVecEnv):
-    def __init__(self, envs):
+    def __len__(self):
+        return self.n_envs
 
+    def getattrs(self, attrname: str, id: list=None):
+        '''Retrieve attributes from each environment
 
+        Args:
+            attr_name (str): Attribute name
+            id (list, optional): Environment indices. Defaults to None.
 
-class DummyVecEnv(VecEnv):
-    def __init__(self, env_fns):
+        Returns:
+            list: A list of retrieved attributes
+        '''
+        self._assert_not_closed()
+        ids = self._get_ids(id)
+        return [self.workers[id].getattr(attr_name) for id in ids]
+
+    def setattrs(self, attr_name: str, value, id: list=None):
+        '''Set attributes of all environments
+
+        Args:
+            attr_name (str): Attribute name
+            value (Any): Attribute value. This value is used accross all envs
+                specified by `id`.
+            id (list, optional): Environment indices. Defaults to None.
+
+        Returns:
+            list: Return values
+        '''
+        self._assert_not_closed()
+        ids = self._get_ids(id)
+        return [self.workers[id].setattr(attr_name, value) for id in ids]
+
+    def reset(self, id: list=None, **kwargs):
+        self._assert_not_closed()
+        ids = self._get_ids(id)
+        obs_list = [self.workers[id].reset(**kwargs) for id in ids]
+        obs = self._flatten_obs(obs_list)
+        self.rms_norm.update(obs)
+        return self.rms_norm(obs)
+
+    def step(self, acts: np.ndarray, id:list=None):
+        self._assert_not_closed()
+        ids = self._get_ids(id)
+        assert len(acts) == len(ids)
+        # step workers
+        [self.workers[id].step_async(act) for act, id in zip(acts, ids)]
+        results = [self.workers[id].step_wait() for id in ids]
+        obs_list,rew_list,done_list,info_list = zip(*results)
+        # stack results
+        obs  = self._flatten_obs(obs_list)
+        rew  = np.stack(rew_list)
+        done = np.stack(done_list)
+        # rms norm
+        self.rms_norm.update(obs)
+        return self.rms_norm(obs), rew, done, info_list
+
+    def seed(self, seed:list=None, id:list=None):
+        self._assert_not_closed()
+        ids = self._get_ids(id)
+        if seed is None:
+            seed_list = [None] * self.n_envs
+        elif isinstance(seed, int):
+            seed_list = [seed+id for id in ids]
+        else:
+            # list, tuple, np.ndarray
+            assert len(seed) == len(ids)
+            seed_list = seed
+        return [self.workers[id].seed(s) for id, s in zip(ids, seed_list)]
+
+    def render(self, **kwagrs):
+        self._assert_not_closed()
+        return [w.render(**kwargs) for w in self.workers]
+
+    def close(self):
+        if self.closed:
+            return
+        [w.close_async() for w in self.workers]
+        [w.close_wait()  for w in self.workers]
+        self.closed = True
+
+    def load(self, path):
+        '''Load rms value from path'''
+        self.rms_norm.load(path)
+        return self
+    
+    def save(self, path):
+        '''Save rms value to path'''
+        self.rms_norm.save(path)
+
+    def _flatten_obs(self, obs_list):
+        obs = ub_utils.nested_iter_tuple(
+            tuple(obs_list), lambda obs: np.stack(obs))
+        return obs
+
+    def _get_ids(self, id: list=None):
+        if id is None:
+            return list(range(self.n_envs))
+        return [id] if np.isscalar(id) else id
+
+    def _assert_not_closed(self):
+        assert not self.closed, "This env is already closed"
+
+    def _get_rms_norm_opt(self, opt):
+        if isinstance(opt, ub_utils.RMSNormalizer):
+            # setup RMS normalizer
+            if not opt.is_setup:
+                opt.setup(self.observation_space)
+            return opt
+        # select default RMS normalizer
+        if isinstance(self.observation_space, 
+                (gym.spaces.Dict, gym.spaces.Tuple)):
+            _rms_class = ub_utils.NestedRMSNormalizer
+        else:
+            _rms_class = ub_utils.RMSNormalizer
+        enable = None
+        if isinstance(opt, bool):
+            enable = opt
+        # Create RMSNormalizer
+        rms = _rms_class(space  = self.observation_space, 
+                         enable = enable)
+        if isinstance(opt, str):
+            rms.load(opt)
+        return rms
--- unstable_baselines/lib/envs/vec/subproc.py
+++ unstable_baselines/lib/envs/vec/subproc.py
@@ -1,22 +1,145 @@
 # --- built in ---
+import os
+import sys
+import enum
+import time
+import logging
+import multiprocessing
+
+from typing import Union
 
 # --- 3rd party ---
-
+import gym
+import cloudpickle
+import numpy as np
 
 # --- my module ---
-from unstable_baselines.lib.envs.vec import base
+from unstable_baselines.lib import utils as ub_utils
+from unstable_baselines.lib.envs.vec import base as vec_base
 
 __all__ = [
-    'SubprocVecEnvWorker',
     'SubprocVecEnv'
 ]
 
 
-class SubprocVecEnvWorker(base.BaseVecEnvWorker):
-    def __init__(self):
-        pass
+class CloudpickleWrapper():
+    def __init__(self, fn):
+        self.fn = fn
+
+    def __getstate__(self):
+        return cloudpickle.dumps(self.fn)
+
+    def __setstate__(self, fn):
+        self.fn = cloudpickle.loads(fn)
+
+# Commands
+class CMD:
+    getattr = 1
+    setattr = 2
+    reset   = 3
+    step    = 4
+    seed    = 5
+    render  = 6
+    close   = 7
 
 
-class SubprocVecEnv(base.BaseVecEnv):
-    def __init__(self):
-        pass
+def _worker(_p, p, env_fn_wrapper):
+    _p.close()
+    env = env_fn_wrapper.fn()
+    try:
+        while True:
+            try:
+                cmd, data = p.recv()
+            except EOFError: # the pipe has been closed
+                p.close()
+                break
+            if cmd == CMD.getattr:
+                p.send(getattr(env, data, None))
+            elif cmd == CMD.setattr:
+                p.send(setattr(env, data[0], data[1]))
+            elif cmd == CMD.reset:
+                p.send(env.reset(**data))
+            elif cmd == CMD.step:
+                obs, rew, done, info = env.step(data)
+                p.send((obs, rew, done, info))
+            elif cmd == CMD.render:
+                p.send(env.render(**data))
+            elif cmd == CMD.close:
+                p.send(env.close())
+                p.close()
+                break
+            else:
+                p.close()
+                raise NotImplementedError
+    except KeyboardInterrupt:
+        p.close()
+
+
+class SubprocEnvWorker(vec_base.BaseEnvWorker):
+    def __init__(self, env_fn):
+        methods = multiprocessing.get_all_start_methods()
+        start_method = 'spawn'
+        if 'forkserver' in methods:
+            start_method = 'forkserver'
+        ctx = multiprocessing.get_context(start_method)
+        self.p, _p = ctx.Pipe()
+        args = (
+            self.p, _p, CloudpickleWrapper(env_fn)
+        )
+        self.process = ctx.Process(traget=_worker, args=args, daemon=True)
+        self.process.start()
+        self._waiting_cmd = None
+        _p.close()
+        super().__init__(env_fn)
+        
+    def getattr(self, attrname: str):
+        return self._cmd(CMD.getattr, attrname)
+
+    def setattr(self, attrname: str, value):
+        return self._cmd(CMD.setattr, attrname, value)
+
+    def reset(self, **kwargs):
+        return self._cmd(CMD.reset, kwargs)
+
+    def step_async(self, act):
+        return self._cmd(CMD.step, act, wait=False)
+
+    def step_wait(self):
+        return self._wait(CMD.step)
+
+    def seed(self, seed):
+        super().seed(seed)
+        return self._cmd(CMD.seed, seed)
+
+    def render(self, **kwargs):
+        return self._cmd(CMD.render, kwargs)
+
+    def close_async(self):
+        return self._cmd(CMD.close, wait=False)
+
+    def close_wait(self):
+        return self._wait(CMD.close)
+    
+    def _cmd(self, cmd, *args, wait=True):
+        if self._waiting_cmd:
+            raise RuntimeError('Another command was sent when '
+                f'waiting for the reply. CMD: {self._waiting_cmd}')
+        self.p.send([cmd, args])
+        self._waiting_cmd = cmd # marked as waiting reply
+        if wait:
+            return self._wait(cmd)
+
+    def _wait(self, cmd):
+        if self._waiting_cmd != cmd:
+            raise RuntimeError
+        res = self.p.recv()
+        self._waiting_cmd = None #unmarked
+        return res
+
+
+class SubprocVecEnv(vec_base.BaseVecEnv):
+    def __init__(self,
+        env_fns: list,
+        rms_norm: Union[str, bool, ub_utils.RMSNormalizer] = None
+    ):
+        super().__init__(env_fns, SubprocEnvWorker, rms_norm)
